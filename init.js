f20ae494d41b49f07afee84b4dd0f6867c4a185a
Dan Kelly <myself@danielkelly.me>
2017-11-13 22:59:46 -0500

    Initial site done :)

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
-const TWEETCHAIN_API = "https://api.tweetchain.info:8443";
+var TWEETCHAIN_API = "https://api.tweetchain.info:8443";
+// var TWEETCHAIN_API = "https://localhost:8443";
 
 $(document).ready(() => {
 	window.twttr = (function(d, s, id) {
@@ -19,14 +20,34 @@ $(document).ready(() => {
 	} (document, "script", "twitter-wjs"));
 
 	twttr.ready(function() {
-		$.getJSON('/getlatest', {}, function(data) {
+		jQuery.getJSON(TWEETCHAIN_API + '/getlatest?count=20&start=0', {}, function(data) {
+				// Create and place
 				twttr.widgets.createTweet(
-					data.id,
+					data[0].id,
 					document.getElementById('latestBlock'),
 					{
 						theme: 'light'
 					}
 				);
+
+				// Proceed to fill the table with the rest of the tweets
+				var blocksDIV = $('div#longestValidChain>table>tbody');
+				for(var block of data) {
+					var linktext = location.protocol + '//twitter.com/' + block.Twitter_user_screen_name + '/status/' + block.id;
+					blocksDIV.append($('<tr />').append([
+						$('<td />', { text: block.block_number, }),
+						$('<td />', { text: block.Twitter_user_screen_name, }),
+						$('<td />', { text: block.text, }),
+						$('<td />', { text: block.Twitter_created_at, }),
+						$('<td />').append([
+							$('<a />', {
+								href: linktext,
+								target: '_blank',
+								text: linktext,
+							}),
+						]),
+					]));
+				}
 			});
 	});
 });
